cd09289bcac18c20a4fd506d6a875763e2cdbbb6
Kasia-Sikora <sikora.katarzyna.waw@gmail.com>
2026-07-07 13:21:45 +0200

    fix: fix cors header production url

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const corsHeaders = {
   "Access-Control-Allow-Origin":
     process.env.NODE_ENV === "production"
-      ? "https://shopping-list-manager.vercel.app"
+      ? "https://shopping-list-manager-seven.vercel.app/"
       : "http://localhost:5173",
   "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type",
